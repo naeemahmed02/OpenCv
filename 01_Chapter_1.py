91d3e921cd8769d84fc3f5859948a2aaecc41851
Naeem Ahmed <naeemahmedbdn2002@gmail.com>
2024-03-05 18:10:02 +0500

    Update 01_Chapter_1.py

--- 01_Chapter_1.py
+++ 01_Chapter_1.py
@@ -1,18 +1,19 @@
-# Import Library
+##### Import Library
 import cv2 as cv
 
-# Loading image
+##### Loading image
 image = cv.imread("naeem.jpg")
 
-# Check if the image is successfully loaded
+##### Check if the image is successfully loaded
 if image is not None:
-    # Display the image in a window titled "Test Image"
+    ##### Display the image in a window titled "Test Image"
     cv.imshow("Test Image", image)
 
-    # Wait for a key press indefinitely
+    ##### Wait for a key press indefinitely
     cv.waitKey(0)
 
-    # Close the window and release resources
+    ##### Close the window and release resources
     cv.destroyAllWindows()
 else:
+    ##### Print a message if the image loading failed
     print("Failed to load the image.")
